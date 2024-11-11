499f04cee6eb3b5e464f8366713ab23219d5379e
Yufan Ma <1392693274@qq.com>
2024-11-11 23:23:05 +0800

    fix some mistakes

--- src/igrf/igrfpy/igrf13.py
+++ src/igrf/igrfpy/igrf13.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import json
 import os
 import datetime
@@ -56,7 +57,7 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
         - The first dimension (size 4) represents magnetic field components:
             - `[0]`: `B_north` – North component (nT) if `isv = 0`, nT/year if `isv = 1`.
             - `[1]`: `B_east` – East component (nT) if `isv = 0`, nT/year if `isv = 1`.
-            - `[2]`: `B_vert` – Vertical (downward) component (nT) if `isv = 0`, nT/year if `isv = 1`.
+            - `[2]`: `B_vert` – Vertical (earthward) component (nT) if `isv = 0`, nT/year if `isv = 1`.
             - `[3]`: `B_total` – Total intensity (nT) if `isv = 0`, nT/year if `isv = 1`.
         - The second dimension (size N) corresponds to the number of input points.
 
@@ -65,18 +66,18 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
     Single point calculation for a specific date:
     >>> IGRF13(30.0, 232.0, 35786, 2016.5)
     Output:
-    [[83.96967257]
-     [ 4.61629875]
-     [12.50244384]
-     [85.02074586]]
+    [[ 85.76211027]
+    [ 17.57562939]
+    [123.91279388]
+    [151.71823487]]
 
     Multiple points and dates:
     >>> IGRF13([30.0, 40.0], [232.0, 250.0], [35786, 40000], ['20160708', '20170809'])
     Output:
-    [[81.27769572 52.32187034]
-     [ 3.42999051  3.554509  ]
-     [18.78557498 23.96335595]
-     [83.49087665 57.65808771]]
+    [[ 81.3243117   52.36266943]
+    [  9.52994813   6.89129736]
+    [124.99133933 113.81947292]
+    [149.42321939 125.47594011]]
     
     
     """
@@ -91,7 +92,6 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
         lat =[lat]
         lon = [lon]
         alt = [alt]
-    int_v = np.vectorize(int)
     lat, lon, alt, dates = map(np.array,[lat, lon, alt, dates])
 
     # Check if all input arrays have the same shape
@@ -131,7 +131,7 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
                   0.0 if isv==1 else 1.0,
                   -0.2 if isv==1 else 1.0-t)
 
-    nmx = np.where(dates<1995, 10, 13)
+    nmx = np.where(dates<2000, 10, 13)
     kmx   = (nmx+1)*(nmx+2)/2
     
 
@@ -184,8 +184,8 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
 
         valid_mask = k <= kmx
         reset_mask = (n < m) & valid_mask
-        m = int_v(np.where(reset_mask, 0, m))
-        n = int_v(np.where(reset_mask, n+1, n))
+        m = np.int32(np.where(reset_mask, 0, m))
+        n = np.int32(np.where(reset_mask, n+1, n))
         rr = np.where(reset_mask, rr*ratio, rr)
         fn = np.where(reset_mask, n, fn)
         gn = np.where(reset_mask, n-1, gn)
@@ -196,7 +196,7 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
         equal_and_not2_mask = equal_mask & (k != 2) & valid_mask
         if np.any(equal_and_not2_mask):
             one[equal_and_not2_mask] = np.sqrt(1.0 - 0.5 / fm[equal_and_not2_mask])
-        j = int_v(np.where(equal_and_not2_mask, k-n-1, 0))
+        j = np.int32(np.where(equal_and_not2_mask, k-n-1, 0))
         p[:,k] = np.where(equal_and_not2_mask, 
                           one * st *p[np.arange(len(j)),j], 
                           p[:,k])
@@ -218,7 +218,7 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
             two[notequal_mask] = np.sqrt(gn**2 - gmm) / one[notequal_mask]
             three[notequal_mask] = (fn + gn) / one[notequal_mask]
 
-        i = int_v(np.where(notequal_mask, k - n, 0)) 
+        i = np.int32(np.where(notequal_mask, k - n, 0)) 
         j = np.where(notequal_mask, i - n + 1, j)
         p[:,k] = np.where(notequal_mask, 
                           three * ct * p[np.arange(len(i)),i] - two * p[np.arange(len(i)),j], 
@@ -227,7 +227,7 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
                           three * (ct * q[np.arange(len(i)),i] - st * p[np.arange(len(i)),i]) - two * q[np.arange(len(j)),j], 
                           q[:,k])
         
-        lm = int_v(l-1)
+        lm = np.int32(l-1)
         one = (tc * gh_values_before[np.arange(len(lm)),lm]+ t * gh_values_after[np.arange(len(lm)),lm]) * rr
         
         noteuqal0_mask = m != 0
@@ -242,23 +242,25 @@ def IGRF13(lat, lon, alt, dates, isv=0, itype=1):
                             np.squeeze(one * q[:,k]))
         B_vert -= np.where(noteuqal0_mask, 
                            np.squeeze((fn + 1.0) * three * p[:,k]), 
-                           np.squeeze((fn + 1.0) * three * p[:,k]))
+                           np.squeeze((fn + 1.0) * one * p[:,k]))
 
-        less_mask = st-0 < 10e-10
+        less_mask = st-0 < 10e-8
         noteuqal0_and_less_mask = noteuqal0_mask & less_mask
+        noteuqal0_and_greater_mask = noteuqal0_mask & ~less_mask
         if np.any(noteuqal0_and_less_mask):
             B_east += np.where(noteuqal0_mask & less_mask, 
-                               (one * sl[np.arange(len(m)),m-1] - two * cl[np.arange(len(m)),m-1]) * fm * p[:,k] / st,
-                               0)
-        B_east += np.where(noteuqal0_mask & ~less_mask, 
                            np.squeeze((one * sl[np.arange(len(m)),m-1] - two * cl[np.arange(len(m)),m-1]) * q[:,k] * ct), 
                            0)
-        l += int_v(np.where(noteuqal0_mask, 2, 1))
+        if np.any(noteuqal0_and_greater_mask):
+            B_east += np.where(noteuqal0_mask & ~less_mask, 
+                               (one * sl[np.arange(len(m)),m-1] - two * cl[np.arange(len(m)),m-1]) * fm * p[:,k] / st,
+                               0)
+        l += np.int32(np.where(noteuqal0_mask, 2, 1))
         m += 1
 
     one = B_north
     B_north = B_north*cd + B_vert*sd
-    B_vert = B_vert - one*sd
+    B_vert = B_vert*cd - one*sd
     B_total = np.sqrt(B_north**2 + B_east**2 + B_vert**2)
     
     return np.array([B_north, B_east, B_vert, B_total])
@@ -302,10 +304,10 @@ def IGRF13_v(data_matrix, isv=0, itype=1):
     -------
     >>> IGRF13_v([[30.0, 232.0, 35786, '20160708'], [40.0, 250.0, 40000, '20170809']])
     Output:
-    [[81.27769572 52.32187034]
-     [ 3.42999051  3.554509  ]
-     [18.78557498 23.96335595]
-     [83.49087665 57.65808771]]
+    [[ 81.3243117   52.36266943]
+    [  9.52994813   6.89129736]
+    [124.99133933 113.81947292]
+    [149.42321939 125.47594011]]
     """
     data_matrix = np.array(data_matrix)
     lat = data_matrix[:, 0].astype(float)
@@ -317,37 +319,47 @@ def IGRF13_v(data_matrix, isv=0, itype=1):
     return result
 
 
+def datetime2yeardec(times):
+    times = pd.to_datetime(times)
+    year = times.year
+    start_of_year = pd.to_datetime(year.astype(str) + '-01-01')
+    next_year = start_of_year + pd.offsets.YearEnd()
+    year_fraction = (times - start_of_year) / (next_year - start_of_year)
+    return year + year_fraction
 
-def datetime2yeardec(time: str | int | np.integer |datetime.datetime | datetime.date) -> float:
-    """
-    Convert a datetime into a float. The integer part of the float should
-    represent the year.
-    Order should be preserved. If adate<bdate, then d2t(adate)<d2t(bdate)
-    time distances should be preserved: If bdate-adate=ddate-cdate then
-    dt2t(bdate)-dt2t(adate) = dt2t(ddate)-dt2t(cdate)
+# def datetime2yeardec(time: str | int | np.integer |datetime.datetime | datetime.date) -> float:
+#     """
+#     Convert a datetime into a float. The integer part of the float should
+#     represent the year.
+#     Order should be preserved. If adate<bdate, then d2t(adate)<d2t(bdate)
+#     time distances should be preserved: If bdate-adate=ddate-cdate then
+#     dt2t(bdate)-dt2t(adate) = dt2t(ddate)-dt2t(cdate)
     
-    """
+#     """
 
-    if isinstance(time, float):
-        # assume already year_dec
-        return time
-    if isinstance(time, str):
-        t = parse(time)
-    elif isinstance(time, (int, np.integer)):
-        t = float(time)
-        return t
-    elif isinstance(time, datetime.datetime):
-        t = time
-    elif isinstance(time, datetime.date):
-        t = datetime.datetime.combine(time, datetime.datetime.min.time())
-    elif isinstance(time, (tuple, list, np.ndarray)):
-        return np.asarray([datetime2yeardec(t) for t in time])
-    else:
-        raise TypeError("unknown input type {}".format(type(time)))
+#     if isinstance(time, float):
+#         # assume already year_dec
+#         return time
+#     if isinstance(time, str):
+#         t = parse(time)
+#     elif isinstance(time, (int, np.integer)):
+#         t = float(time)
+#         return t
+#     elif isinstance(time, datetime.datetime):
+#         t = time
+#     elif isinstance(time, datetime.date):
+#         t = datetime.datetime.combine(time, datetime.datetime.min.time())
+#     elif isinstance(time, np.datetime64):
+#         # Handle a single np.datetime64 element
+#         t = pd.Timestamp(time.astype('datetime64[s]')).to_pydatetime()
+#     elif isinstance(time, (tuple, list, np.ndarray)):
+#         return np.asarray([datetime2yeardec(t) for t in time])
+#     else:
+#         raise TypeError("unknown input type {}".format(type(time)))
 
-    year = t.year
+#     year = t.year
 
-    boy = datetime.datetime(year, 1, 1)
-    eoy = datetime.datetime(year + 1, 1, 1)
+#     boy = datetime.datetime(year, 1, 1)
+#     eoy = datetime.datetime(year + 1, 1, 1)
 
-    return year + ((t - boy).total_seconds() / ((eoy - boy).total_seconds()))
+#     return year + ((t - boy).total_seconds() / ((eoy - boy).total_seconds()))
